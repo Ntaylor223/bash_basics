da972ee70cfcfcc458596330a06e27515baecf92
Nathan <ntaylor14@ggc.edu>
2026-04-01 08:36:15 -0400

    Added alternate song on parallel branch

--- 07-beer.sh
+++ 07-beer.sh
@@ -19,6 +19,29 @@ while [ $count -ge 0 ]; do
   ((count = count - 1))
 
 done
+echo ""
+echo "Now let's sing 5 little ducks"
 
+ducks=5
+
+while [ $ducks -ge 0 ]; do
+    if [ $ducks -gt 1 ]; then
+        echo "$ducks little ducks went out one day"
+        echo "Over the hills and far away"
+        echo "Mother duck said quack quack quack quack"
+    elif [ $ducks -eq 1 ]; then
+        echo "$ducks little duck went out one day"
+        echo "Over the hills and far away"
+        echo "Mother duck said quack quack quack quack"
+    else
+        echo "No little ducks went out one day"
+        echo "Over the hills and far away"
+        echo "Mother duck said quack quack quack quack"
+        echo "And all of the 5 little ducks came back"
+    fi
+
+    echo ""
+    ducks=$((ducks - 1))
+done
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
